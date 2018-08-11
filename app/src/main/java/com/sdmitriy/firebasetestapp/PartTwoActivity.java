9af555053418c5b414e9cd9658fab225d4c3b3bb
troll3758@gmail.com <24081991ukr>
2018-08-11 18:23:20 +0300

    singup fragment

--- app/src/main/java/com/sdmitriy/firebasetestapp/PartTwoActivity.java
+++ app/src/main/java/com/sdmitriy/firebasetestapp/PartTwoActivity.java
@@ -2,6 +2,7 @@ package com.sdmitriy.firebasetestapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -12,8 +13,11 @@ public class PartTwoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_part_two);
+        Window window = getWindow();
+        WindowManager.LayoutParams winParams = window.getAttributes();
+        winParams.flags &= ~WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
+        window.setAttributes(winParams);
+        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        setContentView(R.layout.sign_up_fragment);
     }
 }
